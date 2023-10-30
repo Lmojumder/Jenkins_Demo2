0bbbc08b2e91d7b55234241323088f9c277cadff
mojum <mojum@LAPTOP-C50CP82R.mynetworksettings.com>
2023-10-30 19:08:58 -0400

    hi

---
+++ JenkinDemo_2/src/main/java/com/train/org/JenkinDemo_2/App.java
@@ -0,0 +1,13 @@
+package com.train.org.JenkinDemo_2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ JenkinDemo_2/src/test/java/Test/SampleTest.java
@@ -0,0 +1,23 @@
+package Test;
+
+import org.testng.annotations.Test;
+
+import Utilities.BaseClass;
+
+public class SampleTest extends BaseClass {
+  @Test
+  public void testCase() {
+	  System.out.println("this is Test1");
+	  
+	  
+  }
+  
+  public void testCase2() {
+	  System.out.println("this is Test2");
+  }
+  
+  
+  public void testCase3() {
+	  System.out.println("this is Test3");
+  }
+}
