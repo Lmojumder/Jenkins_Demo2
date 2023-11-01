7c5be8cd99584c3566fa3153a5a6fce3e50bf03b
mojum <mojum@LAPTOP-C50CP82R.mynetworksettings.com>
2023-10-31 23:26:13 -0400

    hi commit 3

--- JenkinDemo_2/src/test/java/Test/SampleTest.java
+++ JenkinDemo_2/src/test/java/Test/SampleTest.java
@@ -11,12 +11,12 @@ public class SampleTest extends BaseClass {
 	  
 	  
   }
-  
+  @Test
   public void testCase2() {
 	  System.out.println("this is Test2");
   }
   
-  
+  @Test
   public void testCase3() {
 	  System.out.println("this is Test3");
   }
